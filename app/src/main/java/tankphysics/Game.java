47fc8d5581251baec9420596a31b126945362a42
apjg1 <apjg1@st-andrews.ac.uk>
2023-01-27 14:55:21 +0000

    Processing successful setup

--- app/src/main/java/tankphysics/Game.java
+++ app/src/main/java/tankphysics/Game.java
@@ -1,5 +1,17 @@
-public class Game {
+package tankphysics;
+
+import processing.core.PApplet;
+
+public class Game extends PApplet {
+	public void settings() {
+		size(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+
+	public void draw() {
+
+	}
+
 	public static void main(String[] args) {
-		return;
+		PApplet.main(args);
 	}
 }
