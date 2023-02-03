669c7f43af0b63d52c77e18d4391068c46017fe1
apjg1 <apjg1@st-andrews.ac.uk>
2023-02-03 18:56:01 +0000

    refactor: Cleaning GameObjects + plane

--- app/src/main/java/tankphysics/Game.java
+++ app/src/main/java/tankphysics/Game.java
@@ -5,12 +5,7 @@ import java.util.Arrays;
 
 import processing.core.PApplet;
 import processing.core.PVector;
-import tankphysics.engine.Director;
-import tankphysics.engine.GameObject;
-import tankphysics.engine.RigidBody;
-import tankphysics.engine.Sprite;
-import tankphysics.engine.VisualModel;
-import tankphysics.engine.VisualPolygon;
+import tankphysics.engine.*;
 
 public class Game extends PApplet {
 	Director engineDirector;
@@ -20,6 +15,7 @@ public class Game extends PApplet {
 		engineDirector = new Director(this);
 		camera = engineDirector.getCamera();
 
+		// Set up vertices for blocks
 		ArrayList<PVector> vertices = new ArrayList<>(
 				Arrays.asList(new PVector(0, 1), new PVector(1, 1), new PVector(1, 0), new PVector(0, 0)));
 		ArrayList<PVector> blockUv = new ArrayList<>(Arrays.asList(new PVector(0, 128 * 8),
@@ -27,20 +23,21 @@ public class Game extends PApplet {
 
 		// Make dirt block for show.
 		GameObject dirtBlock = new GameObject(new PVector(512, 512),
-				new PVector(displayWidth - 960, displayHeight - 540));
-		VisualModel blockViz = new VisualPolygon(vertices, new PVector(256, 256), loadImage("dirt_block.png"), blockUv);
+				new PVector(displayWidth - 960, displayHeight - 540), false,
+				new VisualPolygon(vertices, new PVector(256, 256), loadImage("dirt_block.png"), blockUv));
 
 		// Make gravity-bound object.
-		GameObject bullet = new GameObject(new PVector(64, 64), new PVector(128, displayHeight - 128));
 		RigidBody bulletCPU = new RigidBody(18.7f, 0.2f);
-		VisualModel bulletVisual = new Sprite("dirt_block.png", new PVector(32, 32));
+		GameObject bullet = new GameObject(new PVector(64, 64), new PVector(128, displayHeight - 128), false,
+				new Sprite("dirt_block.png", new PVector(32, 32)), bulletCPU);
+
+		// Make a plane for collision checks.
+		GameObject plane = new GameObject(new PVector(1800, 32), new PVector(960, displayHeight - 64), false,
+				new VisualPolygon(vertices, new PVector(900, 16), color(255)),
+				new CollisionMesh(new PVector(1800, 32), new PVector(900, 16), vertices, 1f));
 
 		// Attach all components to director.
-		dirtBlock.attach(blockViz);
-		bullet.attach(bulletCPU);
-		bullet.attach(bulletVisual);
-		engineDirector.attach(dirtBlock);
-		engineDirector.attach(bullet);
+		engineDirector.attach(dirtBlock, plane, bullet);
 
 		// Give initial velocity to bullet.
 		bulletCPU.setVelocity(new PVector(10, -10));
@@ -58,7 +55,7 @@ public class Game extends PApplet {
 			camera.getSize().add(new PVector(displayWidth / 180, displayHeight / 180));
 		}
 
-		engineDirector.draw();
+		engineDirector.nextFrame();
 	}
 
 	public static void main(String[] args) {
