0893bb3786f7647dfcb590869d51de9c1748d9a9
UnikMask <alexandre.guerville@hotmail.com>
2023-02-12 10:12:41 +0000

    feat: Base EventListener interface

---
+++ app/src/main/java/tankphysics/engine/EventListener.java
@@ -0,0 +1,19 @@
+package tankphysics.engine;
+
+/**
+ * Interface for event listeners - interfaces that can be used to call methods
+ * from other game objects during events involving remote GameObjects or on
+ * Director events.
+ */
+public interface EventListener {
+	/**
+	 * Call the event listener.
+	 *
+	 * @param caller     The GameObject that called the event, or null if none or
+	 *                   Director.
+	 * @param parameters Set of parameters that the call takes. Because the
+	 *                   parameters depend on the kind of event, handling of those
+	 *                   is left at caution of the user.
+	 */
+	public void call(GameObject caller, Object... parameters);
+}
