395ec9207b476c9bec25ca34f2857087bc56a327
apjg1 <apjg1@st-andrews.ac.uk>
2023-02-13 19:08:35 +0000

    [Game] Added ceiling, box spacing, blue tank

--- app/src/main/java/tankphysics/Game.java
+++ app/src/main/java/tankphysics/Game.java
@@ -1,5 +1,6 @@
 package tankphysics;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import processing.core.PApplet;
@@ -9,32 +10,39 @@ import tankphysics.engine.*;
 public class Game extends PApplet {
 	Director engineDirector;
 	GameObject camera;
-	Tank tank;
+	Tank redTank;
+	Tank blueTank;
+
+	HashMap<Character, Integer> heldKeys = new HashMap<>();
 
 	public void setup() {
-		frameRate(144);
+		frameRate(60);
 		engineDirector = new Director(this);
 		camera = engineDirector.getCamera();
 
 		// Make a plane for collision checks.
-		GameObject plane = new GameObject(new PVector(28.125f, 0.5f), new PVector(15, 16.625f), false,
-				new VisualPolygon(new PVector(), Polygons.makeSquare(new PVector(28.125f, 0.5f)), color(255)),
-				new CollisionMesh(new PVector(), Polygons.makeSquare(new PVector(28.125f, 0.5f)),
+		GameObject floor = new GameObject(new PVector(30, 5), new PVector(0, 4), false,
+				new VisualPolygon(new PVector(), Polygons.makeSquare(new PVector(30, 5)), color(128)),
+				new CollisionMesh(new PVector(), Polygons.makeSquare(new PVector(30, 5)),
 						Map.ofEntries(Map.entry("staticFriction", 0.6f), Map.entry("dynamicFriction", 0.5f),
 								Map.entry("bounciness", 0.3f))));
-		tank = new Tank(new PVector(3, 14), color(255, 0, 0));
-		GameObject[] boxGrid = new GameObject[9];
-		for (int i = 0; i < 9; i++) {
-			boxGrid[i] = new Box(new PVector(15 + i / 3, 15 - i % 3), this);
+
+		GameObject ceiling = new GameObject(new PVector(30, 5), new PVector(0, -13), false,
+				new VisualPolygon(new PVector(), Polygons.makeSquare(new PVector(30, 5)), color(128)),
+				new CollisionMesh(new PVector(), Polygons.makeSquare(new PVector(30, 5)),
+						Map.ofEntries(Map.entry("staticFriction", 0.6f), Map.entry("dynamicFriction", 0.5f),
+								Map.entry("bounciness", 0.3f))));
+
+		redTank = new Tank(new PVector(-13, -2), color(255, 0, 0));
+		blueTank = new Tank(new PVector(13, -2), color(0, 0, 255));
+		GameObject[] boxGrid = new GameObject[25];
+		for (int i = 0; i < 25; i++) {
+			boxGrid[i] = new Box(new PVector(-6.5f + 3.2f * (i / 5), -2 - 2.1f * (i % 5)), this);
 		}
 
 		// Attach all components to director.
-		engineDirector.attach(tank, plane);
+		engineDirector.attach(floor, ceiling, redTank, blueTank);
 		engineDirector.attach(boxGrid);
-
-		// Set initial camera position and zoom
-		camera.setPosition(tank.getPosition());
-		camera.setSize(new PVector(32, 18));
 	}
 
 	public void settings() {
@@ -43,25 +51,66 @@ public class Game extends PApplet {
 	}
 
 	public void keyReleased() {
+		if (!heldKeys.containsKey(key)) {
+			return;
+		}
+		heldKeys.remove(key);
 		if (key == 'w') {
 			engineDirector.togglePause();
+		}
+		if (key == 'd') {
+			if (!heldKeys.containsKey('a')) {
+				redTank.setIdleFriction();
+			}
+		}
+		if (key == 'a') {
+			if (!heldKeys.containsKey('d')) {
+				redTank.setIdleFriction();
+			}
 		}
 	}
 
 	public void keyPressed() {
+		heldKeys.put(key, 0);
 		if (key == 'd') {
-			tank.getRigidBody().setVelocity(PVector.add(tank.getRigidBody().getVelocity(), new PVector(1, 0)));
+			if (!heldKeys.containsKey('a')) {
+				redTank.setMovingFriction();
+			} else {
+				heldKeys.remove('d');
+			}
 		}
 		if (key == 'a') {
-			tank.getRigidBody().setVelocity(PVector.add(tank.getRigidBody().getVelocity(), new PVector(-1, 0)));
+			if (!heldKeys.containsKey('d')) {
+				redTank.setMovingFriction();
+			} else {
+				heldKeys.remove('a');
+			}
+		}
+	}
+
+	public void keyHeld(char key) {
+		if (key == 'd') {
+			redTank.drive(true);
+		}
+		if (key == 'a') {
+			redTank.drive(false);
 		}
 	}
 
 	public void draw() {
 		background(0);
 
-		camera.setPosition(new PVector(lerp(camera.getPosition().x, tank.getPosition().x, 0.02f),
-				lerp(camera.getPosition().y, tank.getPosition().y, 0.02f)));
+		float scale = max(16, Math.abs(redTank.getPosition().x - blueTank.getPosition().x)) + 4;
+
+		camera.setPosition(PVector.add(PVector.div(PVector.add(redTank.getPosition(), blueTank.getPosition()), 2),
+				new PVector(0, -5)));
+		camera.setSize(new PVector(scale, (9f / 16f) * scale));
+		redTank.getNozzle().setExtraAngle(
+				PVector.sub(new PVector(mouseX, mouseY), engineDirector.getSetVector(redTank.getNozzle().getPosition()))
+						.heading());
+		for (char key : heldKeys.keySet()) {
+			keyHeld(key);
+		}
 		engineDirector.nextFrame();
 	}
 
--- app/src/main/java/tankphysics/Tank.java
+++ app/src/main/java/tankphysics/Tank.java
@@ -7,6 +7,7 @@ import tankphysics.engine.Polygons;
 import tankphysics.engine.VisualPolygon;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Arrays;
 import processing.core.PVector;
 
@@ -18,17 +19,70 @@ public class Tank extends GameObject {
 			new PVector(0.5f, 0.5f), new PVector(-0.5f, 0.5f));
 	private static final List<PVector> hull = Polygons.makeRegularPolygon(new PVector(1, 1), 16, 0,
 			new PVector(0, -0.625f));
-	private static final float TANK_MASS = 80;
-	private RigidBody tankBody = new RigidBody(TANK_MASS, new CollisionMesh(new PVector(), belly, null),
-			new CollisionMesh(new PVector(), hull, null));
+	private static final float TANK_MASS = 800;
+
+	// GameObject bomponents
+	private CollisionMesh bellyMesh = new CollisionMesh(new PVector(), belly, null);
+	private RigidBody tankBody = new RigidBody(TANK_MASS, bellyMesh, new CollisionMesh(new PVector(), hull, null));
+	private Nozzle nozzle;
+
+	// Class that represents the nozzle of the tank.
+	class Nozzle extends GameObject {
+		private static final float MIN_BAD_ANGLE = 0.19f;
+		private static final float MAX_BAD_ANGLE = (float) Math.PI - 0.19f;
+		private static final List<PVector> nozzle = Polygons.makeSquare(new PVector(1.1f, 0.2f),
+				new PVector(0.1f, 0.1f));
+		private float extraAngle = 0;
+
+		@Override
+		public float getRotation() {
+			System.out.println(extraAngle);
+			return super.rotation + extraAngle;
+		}
+
+		public void setExtraAngle(float angle) {
+			if (angle < MIN_BAD_ANGLE || angle > MAX_BAD_ANGLE) {
+				this.extraAngle = angle;
+			} else {
+				this.extraAngle = angle > (MIN_BAD_ANGLE + MAX_BAD_ANGLE) / 2 ? MAX_BAD_ANGLE : MIN_BAD_ANGLE;
+			}
+		}
+
+		Nozzle(PVector position, int color) {
+			super(new PVector(1.5f, 0.3f), position, true, new VisualPolygon(new PVector(), nozzle, color));
+		}
+	}
 
 	public RigidBody getRigidBody() {
 		return tankBody;
 	}
 
+	public Nozzle getNozzle() {
+		return nozzle;
+	}
+
+	public void setMovingFriction() {
+		bellyMesh.setProperties(Map.ofEntries(Map.entry("dynamicFriction", 0f)));
+	}
+
+	public void setIdleFriction() {
+		bellyMesh.setProperties(Map.ofEntries(Map.entry("dynamicFriction", 1f)));
+	}
+
+	public void drive(boolean left) {
+		tankBody.setVelocity(PVector.add(tankBody.getVelocity(),
+				Polygons.getRotatedVector(new PVector(left ? 0.1f : -0.1f, 0), rotation)));
+	}
+
+	//////////////////
+	// Constructors //
+	//////////////////
+
 	public Tank(PVector position, int color) {
 		super(new PVector(1.5f, 2.125f), position, false, new VisualPolygon(new PVector(), belly, color),
 				new VisualPolygon(new PVector(), hull, color));
 		this.attach(tankBody);
+		nozzle = new Nozzle(position, color);
+		this.addChild(nozzle, new PVector(0, -0.750f));
 	}
 }
