207d7d1f37f18bcb41474dbccb430c758093694b
apjg1 <apjg1@st-andrews.ac.uk>
2023-02-13 16:37:10 +0000

    refactor: Tank rigidBody initialisation

--- app/src/main/java/tankphysics/Tank.java
+++ app/src/main/java/tankphysics/Tank.java
@@ -10,13 +10,17 @@ import java.util.List;
 import java.util.Arrays;
 import processing.core.PVector;
 
+/**
+ * Class representing a tank in the tankphysics game.
+ */
 public class Tank extends GameObject {
 	private static final List<PVector> belly = Arrays.asList(new PVector(-0.75f, -0.5f), new PVector(0.75f, -0.5f),
 			new PVector(0.5f, 0.5f), new PVector(-0.5f, 0.5f));
 	private static final List<PVector> hull = Polygons.makeRegularPolygon(new PVector(1, 1), 16, 0,
 			new PVector(0, -0.625f));
 	private static final float TANK_MASS = 80;
-	private RigidBody tankBody;
+	private RigidBody tankBody = new RigidBody(TANK_MASS, new CollisionMesh(new PVector(), belly, null),
+			new CollisionMesh(new PVector(), hull, null));
 
 	public RigidBody getRigidBody() {
 		return tankBody;
@@ -25,10 +29,6 @@ public class Tank extends GameObject {
 	public Tank(PVector position, int color) {
 		super(new PVector(1.5f, 2.125f), position, false, new VisualPolygon(new PVector(), belly, color),
 				new VisualPolygon(new PVector(), hull, color));
-		CollisionMesh bodyMesh = new CollisionMesh(new PVector(), belly, null);
-		CollisionMesh hullMesh = new CollisionMesh(new PVector(), hull, null);
-		tankBody = new RigidBody(TANK_MASS);
-		tankBody.attachToHitbox(bodyMesh, hullMesh);
 		this.attach(tankBody);
 	}
 }
