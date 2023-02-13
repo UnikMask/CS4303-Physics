e8181dbbc602422f45ad54d743007b023cdddabe
apjg1 <apjg1@st-andrews.ac.uk>
2023-02-13 14:54:49 +0000

    Tank redesign

--- app/src/main/java/tankphysics/Tank.java
+++ app/src/main/java/tankphysics/Tank.java
@@ -9,12 +9,12 @@ import tankphysics.engine.VisualPolygon;
 import java.util.List;
 import java.util.Arrays;
 import processing.core.PVector;
-import processing.core.PApplet;
 
 public class Tank extends GameObject {
-	private static final List<PVector> belly = Arrays.asList(new PVector(-1, -0.5f), new PVector(1, -0.5f), new PVector(0.5f, 0.5f),
-			new PVector(-0.5f, 0.5f));
-	private static final List<PVector> hull = Polygons.makeRegularPolygon(new PVector(1, 1), 16);
+	private static final List<PVector> belly = Arrays.asList(new PVector(-0.75f, -0.5f), new PVector(0.75f, -0.5f),
+			new PVector(0.5f, 0.5f), new PVector(-0.5f, 0.5f));
+	private static final List<PVector> hull = Polygons.makeRegularPolygon(new PVector(1, 1), 16, 0,
+			new PVector(0, -0.625f));
 	private static final float TANK_MASS = 80;
 	private RigidBody tankBody;
 
@@ -23,10 +23,10 @@ public class Tank extends GameObject {
 	}
 
 	public Tank(PVector position, int color) {
-		super(new PVector(8, 5.5f), position, false, new VisualPolygon(new PVector(), belly, color),
-				new VisualPolygon(new PVector(0, 0.5f), hull, color));
+		super(new PVector(1.5f, 2.125f), position, false, new VisualPolygon(new PVector(), belly, color),
+				new VisualPolygon(new PVector(), hull, color));
 		CollisionMesh bodyMesh = new CollisionMesh(new PVector(), belly, null);
-		CollisionMesh hullMesh = new CollisionMesh(new PVector(0, 0.5f), hull, null);
+		CollisionMesh hullMesh = new CollisionMesh(new PVector(), hull, null);
 		tankBody = new RigidBody(TANK_MASS);
 		tankBody.attachToHitbox(bodyMesh, hullMesh);
 		this.attach(tankBody);
