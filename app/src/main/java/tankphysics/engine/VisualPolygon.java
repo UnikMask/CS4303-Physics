e5860aa764dbe1e9d777390f44de009c78b1a3f3
apjg1 <apjg1@st-andrews.ac.uk>
2023-02-14 16:48:49 +0000

    feat: Tint and scale modifiers

--- app/src/main/java/tankphysics/engine/VisualPolygon.java
+++ app/src/main/java/tankphysics/engine/VisualPolygon.java
@@ -8,21 +8,40 @@ import processing.core.PShape;
 import processing.core.PVector;
 
 public class VisualPolygon extends VisualModel {
-	PShape polygonShape;
-	List<PVector> vertices;
-	PVector anchor;
-	FillType fillType;
+	private PShape polygonShape;
+	private List<PVector> vertices;
+	private PVector anchor;
+	private FillType fillType;
+	private int tint = 0xFFFFFFFF;
+	private PVector scale = new PVector(1, 1);
 
 	// colour/texture for polygon
-	Integer colour;
-	PImage texture;
-	List<PVector> uv;
+	private Integer colour;
+	private PImage texture;
+	private List<PVector> uv;
 
 	/**
 	 * Enum determining whether a texture or a colour will fill a polygon.
 	 */
 	public static enum FillType {
 		Colour, Texture
+	}
+
+	public PVector getScale() {
+		return scale;
+	}
+
+	public void setScale(PVector scale) {
+		this.scale = scale;
+	}
+
+	public void setTint(int tint) {
+		this.tint = tint;
+		this.polygonShape = null;
+	}
+
+	public int getTint() {
+		return this.tint;
 	}
 
 	public void draw(PApplet sketch) {
@@ -33,6 +52,7 @@ public class VisualPolygon extends VisualModel {
 		sketch.translate(object.getPosition().x, object.getPosition().y);
 		sketch.rotate(object.getRotation());
 		sketch.translate(-anchor.x, -anchor.y);
+		sketch.scale(scale.x, scale.y);
 		sketch.textureWrap(PApplet.REPEAT);
 		sketch.shape(polygonShape, 0, 0);
 		sketch.popMatrix();
@@ -56,6 +76,7 @@ public class VisualPolygon extends VisualModel {
 		} else if (fillType == FillType.Texture) {
 			polygonShape.textureMode(PApplet.IMAGE);
 			polygonShape.texture(texture);
+			polygonShape.tint(tint);
 			for (int i = 0; i < uv.size(); i++) {
 				polygonShape.vertex(vertices.get(i).x, vertices.get(i).y, uv.get(i).x, uv.get(i).y);
 			}
