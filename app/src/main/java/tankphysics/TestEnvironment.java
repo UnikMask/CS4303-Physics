429de6a7a897bccae559563ceb9958107965dcd0
apjg1 <apjg1@st-andrews.ac.uk>
2023-02-14 13:08:54 +0000

    [TestEnv] particle testing

--- app/src/main/java/tankphysics/TestEnvironment.java
+++ app/src/main/java/tankphysics/TestEnvironment.java
@@ -6,6 +6,7 @@ import tankphysics.engine.CollisionMesh;
 import tankphysics.engine.Director;
 import tankphysics.engine.GameObject;
 import tankphysics.engine.Polygons;
+import tankphysics.engine.RigidBody;
 import tankphysics.engine.Surfaces;
 import tankphysics.engine.VisualPolygon;
 
@@ -18,11 +19,15 @@ public class TestEnvironment extends PApplet {
 		testDirector = new Director(this);
 		camera = testDirector.getCamera();
 
-		GameObject floor = new GameObject(new PVector(30, 5), new PVector(0, 0), false,
-				new VisualPolygon(new PVector(), Polygons.makeSquare(new PVector(30, 5)), color(128)),
-				new CollisionMesh(new PVector(), Polygons.makeSquare(new PVector(30, 5)), Surfaces.getRoughSurface()));
+		GameObject floor = new GameObject(new PVector(30, 2), new PVector(0, 0), false,
+				new VisualPolygon(new PVector(), Polygons.makeSquare(new PVector(30, 2)), color(128)),
+				new CollisionMesh(new PVector(), Polygons.makeSquare(new PVector(30, 2)), Surfaces.getRoughSurface()));
 
-		testDirector.attach(floor);
+		particle = new GameObject(new PVector(1, 1), new PVector(0, -8), false,
+				new VisualPolygon(new PVector(0.5f, 0.5f), Polygons.makeRegularPolygon(new PVector(1, 1), 26), 255),
+				new RigidBody(3, new CollisionMesh(null)));
+
+		testDirector.attach(floor, particle);
 	}
 
 	public void settings() {
