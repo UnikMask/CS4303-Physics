6e5331253354d280033da2062a836754e675b1e7
apjg1 <apjg1@st-andrews.ac.uk>
2023-02-14 12:09:51 +0000

    feat: Common surfaces

---
+++ app/src/main/java/tankphysics/engine/Surfaces.java
@@ -0,0 +1,17 @@
+package tankphysics.engine;
+
+import java.util.HashMap;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+
+/* Collection of surface properties for collision meshes */
+public class Surfaces {
+	public static HashMap<String, Float> getRoughSurface() {
+		return Maps.newHashMap(ImmutableMap.of("staticFriction", 0.6f, "dynamicFriction", 0.5f, "bounciness", 0.3f));
+	}
+
+	public static HashMap<String, Float> getWoodenSurface() {
+		return Maps.newHashMap(ImmutableMap.of("bounciness", 0.1f, "staticFricion", 1.2f, "dynamicFriction", 1f));
+	}
+}
