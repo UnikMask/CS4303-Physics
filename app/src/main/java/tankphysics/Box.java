aae0107d0f48042accf2c721ecb0d30d546f1815
apjg1 <apjg1@st-andrews.ac.uk>
2023-02-14 16:48:59 +0000

    feat: Box collision and destruction

--- app/src/main/java/tankphysics/Box.java
+++ app/src/main/java/tankphysics/Box.java
@@ -18,16 +18,38 @@ public class Box extends GameObject {
 	private RigidBody rigidBody = new RigidBody(BOX_MASS,
 			new CollisionMesh(new PVector(), Polygons.makeSquare(new PVector(1.5f, 1.5f)), boxSurface));
 	private static final float BOX_MASS = 20;
+	private static final int DESTRUCTION_FRAME_COUNT = 25;
+	private static final PVector DESTRUCTION_FINAL_SIZE = new PVector(16, 16);
+	private Integer numDestructionFrames = 0;
+	private VisualPolygon boxLooks;
 
 	public EventListener getBoxOnHitListener(Director engineDirector) {
+		return new EventListener() {
+			public void call(GameObject caller, Object... parameters) {
+				if (caller instanceof Bullet) {
+					engineDirector.disattach(caller);
+					engineDirector.disattachComponent(rigidBody);
+					engineDirector.attachEventListener("update", getBoxDestructionListener(engineDirector));
+				}
+			}
+		};
+	}
+
+	public EventListener getBoxDestructionListener(Director engineDirector) {
 		GameObject self = this;
 		return new EventListener() {
 			public void call(GameObject caller, Object... parameters) {
-				System.out.println("Hit!");
-				if (caller instanceof Bullet) {
-					engineDirector.disattach(caller);
+				numDestructionFrames++;
+
+				if (numDestructionFrames >= DESTRUCTION_FRAME_COUNT) {
 					engineDirector.disattach(self);
+					engineDirector.disattachEventListener(this);
+				} else {
+					boxLooks.setScale(PVector.lerp(boxLooks.getScale(), DESTRUCTION_FINAL_SIZE, 0.05f));
+					int tint = (boxLooks.getTint() >>> 24);
+					boxLooks.setTint(((int) PApplet.lerp(tint, 0, 0.1f)) << 24 | 0x00FFFFFF);
 				}
+
 			}
 		};
 	}
@@ -37,12 +59,13 @@ public class Box extends GameObject {
 	}
 
 	public Box(PVector position, PApplet sketch, Director engineDirector) {
-		super(new PVector(1.5f, 1.5f), position, false,
-				new VisualPolygon(new PVector(), Polygons.makeSquare(new PVector(1.5f, 1.5f)),
-						sketch.loadImage("box_texture.jpg"),
-						Polygons.getPolygonUVMapping(Polygons.makeSquare(new PVector(1.5f, 1.5f)),
-								new PVector(-0.75f, -0.75f), new PVector(0.75f, 0.75f), new PVector(480, 480))));
+		super(new PVector(1.5f, 1.5f), position, false);
+		boxLooks = new VisualPolygon(new PVector(), Polygons.makeSquare(new PVector(1.5f, 1.5f)),
+				sketch.loadImage("box_texture.jpg"),
+				Polygons.getPolygonUVMapping(Polygons.makeSquare(new PVector(1.5f, 1.5f)), new PVector(-0.75f, -0.75f),
+						new PVector(0.75f, 0.75f), new PVector(480, 480)));
 		attachEventListener("onHit", getBoxOnHitListener(engineDirector));
 		attach(rigidBody);
+		attach(boxLooks);
 	}
 }
