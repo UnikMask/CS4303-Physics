e947c7195c27619fd81a57b0a0e25e191ecbc056
apjg1 <apjg1@st-andrews.ac.uk>
2023-02-16 12:00:28 +0000

    fix: Load Box texture a single time.

--- app/src/main/java/tankphysics/Box.java
+++ app/src/main/java/tankphysics/Box.java
@@ -3,6 +3,7 @@ package tankphysics;
 import java.util.Map;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 import processing.core.PVector;
 import tankphysics.engine.CollisionMesh;
 import tankphysics.engine.Director;
@@ -13,15 +14,21 @@ import tankphysics.engine.RigidBody;
 import tankphysics.engine.VisualPolygon;
 
 public class Box extends GameObject {
+	// Constants
 	private static final Map<String, Float> boxSurface = Map.ofEntries(Map.entry("bounciness", 0.3f),
 			Map.entry("staticFriction", 1.2f), Map.entry("dynamicFriction", 1f));
-	private RigidBody rigidBody = new RigidBody(BOX_MASS,
-			new CollisionMesh(new PVector(), Polygons.makeSquare(new PVector(1.5f, 1.5f)), boxSurface));
 	private static final float BOX_MASS = 20;
 	private static final int DESTRUCTION_FRAME_COUNT = 25;
 	private static final PVector DESTRUCTION_FINAL_SIZE = new PVector(4, 4);
+	private static final String BOX_TEXTURE_PATH = "box_texture.jpg";
+	private static final PVector TEXTURE_SIZE = new PVector(480, 480);
+
+	// Box attributes
+	private static PImage boxTexture = null;
 	private Integer numDestructionFrames = 0;
 	private VisualPolygon boxLooks;
+	private RigidBody rigidBody = new RigidBody(BOX_MASS,
+			new CollisionMesh(new PVector(), Polygons.makeSquare(new PVector(1.5f, 1.5f)), boxSurface));
 
 	public EngineEventListener getBoxOnHitListener(Director engineDirector) {
 		return new EngineEventListener() {
@@ -57,10 +64,14 @@ public class Box extends GameObject {
 
 	public Box(PVector position, PApplet sketch, Director engineDirector) {
 		super(new PVector(1.5f, 1.5f), position, false);
-		boxLooks = new VisualPolygon(new PVector(), Polygons.makeSquare(new PVector(1.5f, 1.5f)),
-				sketch.loadImage("box_texture.jpg"),
+
+		if (Box.boxTexture == null) {
+			Box.boxTexture = sketch.loadImage(BOX_TEXTURE_PATH);
+		}
+
+		boxLooks = new VisualPolygon(new PVector(), Polygons.makeSquare(new PVector(1.5f, 1.5f)), Box.boxTexture,
 				Polygons.getPolygonUVMapping(Polygons.makeSquare(new PVector(1.5f, 1.5f)), new PVector(-0.75f, -0.75f),
-						new PVector(0.75f, 0.75f), new PVector(480, 480)));
+						new PVector(0.75f, 0.75f), TEXTURE_SIZE));
 		attachEventListener("onHit", getBoxOnHitListener(engineDirector));
 		attach(rigidBody);
 		attach(boxLooks);
